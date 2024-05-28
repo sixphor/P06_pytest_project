1052c317afc3ce7e1459f955220195903f689c10
ariq <23016438@myrp.edu.sg>
2024-05-28 15:45:35 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,70 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange
+        a = 4
+        b = 1
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 3
+        assert result == expected
+
+    def test_multiply(self):
+        # arrange
+        a = 4
+        b = 3
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 12
+        assert result == expected
+    
+    def test_divide(self):
+            # arrange
+            a = 12
+            b = 3
+            cal = Calculator()
+
+            # act
+            result = cal.divide(a, b)
+
+            # assert
+            expected = 4
+            assert result == expected
+
+    def test_divide_zerodiv(self):
+        # arrange
+        a = 12
+        b = 0
+        cal = Calculator()
+
+        # act
+        try:
+            result = cal.divide(a, b)
+        except ZeroDivisionError:
+            pass
+
+
+
+
